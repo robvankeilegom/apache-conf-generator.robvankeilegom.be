27cc0bd3a92f4955c621305e8c5965c9ffaa56e5
Rob Van Keilegom <robvankeilegom@hotmail.com>
2020-04-28 17:01:30 +0200

    Consitent naming

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-ng build --prod
-rsync -av ./dist/apache-config-generator/ user@robvankeilegom.be:~/web/apache-config-generator.robvankeilegom.be/
+./node_modules/.bin/ng build --prod
+rsync -av ./dist/apache-conf-generator/ user@robvankeilegom.be:~/web/apache-conf-generator.robvankeilegom.be/

ccbd63f60dce0748f548699544cbafa071e3cb3d
Rob Van Keilegom <robvankeilegom@hotmail.com>
2021-05-30 10:02:58 +0200

    :rocket: Upgrade deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-./node_modules/.bin/ng build --prod
+./node_modules/.bin/ng build --configuration production
 rsync -av ./dist/apache-conf-generator/ apache-conf-generator.robvankeilegom.be:~/webroot/apache-conf-generator.robvankeilegom.be/ --delete
 
